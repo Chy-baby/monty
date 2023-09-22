1cb535719fcc5e32291781197cbd54970de24bb9
Ogbenna Nmesoma Anita <132371463+s0m-a@users.noreply.github.com>
2023-09-22 02:04:02 +0100

    Update connector.c
    error handling

--- connector.c
+++ connector.c
@@ -39,45 +39,6 @@ exit(EXIT_FAILURE);
 }
 
 /**
- * calls - calls
- * return: Returns nothing(void)
- */
-
-void calls(void)
-{
-instruction_t instruct[] = {
-{"push", push},
-{"pall", pall},
-{NULL, NULL}
-};
-
-int a = 0;
-char *x = NULL;
-
-x = strtok(args.i_line, " \n");
-
-if (!x || *x == '#')
-{
-return;
-}
-
-while (instruct[a].opcode && x)
-{
-if (!strcmp(x, instruct[a].opcode))
-{
-instruct[a].f(&(args.d_list), args.count);
-return;
-}
-a++;
-}
-
-fprintf(stderr, "L%ld: unknown instruction %s\n", args.count, x);
-clears();
-exit(EXIT_FAILURE);
-}
-
-
-/**
  * shiftUp - shifts stack to the top
  * @stack: double linkedlist
  * @line_number:line num
